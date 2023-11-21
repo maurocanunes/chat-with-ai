50791ae3441c434ba6f3053b0d9b68519186c917
Mauro Nunes <mauro.correa.nunes@gmail.com>
2023-11-20 22:03:10 -0300

    dangerouslyAllowBrowser: true

--- src/Components/Chat/Conversation.js
+++ src/Components/Chat/Conversation.js
@@ -8,7 +8,7 @@ import InputGroup from 'react-bootstrap/InputGroup';
 
 
 const Conversation = () => {
-    const openai = new OpenAI({apiKey: process.env.REACT_APP_API_KEY});
+    const openai = new OpenAI({apiKey: process.env.REACT_APP_API_KEY, dangerouslyAllowBrowser: true});
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState('');
     const [conversation, setConversation] = useState([]);
